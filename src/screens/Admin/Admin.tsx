37d5f78e67e8b89e8539990859f84446eb192c38
lidordvir <lidor4001@gmail.com>
2023-12-07 16:43:01 +0200

    * Added create movie button in admin screen

--- src/screens/Admin/Admin.tsx
+++ src/screens/Admin/Admin.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useCallback, useEffect } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import { IRootState } from "../../store/reducers/combineReducer.reducer";
 import { onMount } from "./Admin.controller";
@@ -9,10 +9,14 @@ import { CancelMovieScheduleModal } from "./modals/CancelMovieScheduleModal/Canc
 import { AddMovieScheduleModal } from "./modals/AddMovieScheduleModal/AddMovieScheduleModal";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import styles from "./Admin.module.scss";
+import { useNavigate } from "react-router-dom";
 
 interface IProps {}
 
 export const Admin: FC<IProps> = React.memo(({}) => {
+  const navigate = useNavigate();
+
   const isLoading = useSelector(
     (state: IRootState) => state.admin.requests.moviesScheduleRequest.isLoading,
     shallowEqual
@@ -26,8 +30,21 @@ export const Admin: FC<IProps> = React.memo(({}) => {
     onMount();
   }, []);
 
+  const _onClickCreateMovie = useCallback(() => {
+    navigate("/admin/movie/create");
+  }, [navigate]);
+
   return (
     <>
+      <div className={styles.createMovieButtonContainer}>
+        <button
+          className={styles.createMovieButton}
+          onClick={_onClickCreateMovie}
+        >
+          <h1>Create Movie</h1>
+        </button>
+      </div>
+
       <>
         {isLoading ? (
           <Loader />
