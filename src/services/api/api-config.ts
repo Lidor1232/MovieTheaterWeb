c531d365a6eea13b046096a2e3112839cf1bb3ac
lidordvir <lidor4001@gmail.com>
2023-12-08 16:00:48 +0200

    * Update BASE_URL to prod url * Add README.md file

--- src/services/api/api-config.ts
+++ src/services/api/api-config.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  "http://movietheater-env.eba-ep4wnv7m.eu-central-1.elasticbeanstalk.com";
 
 export const movieTheaterAxios = axios.create({
   baseURL: BASE_URL,
